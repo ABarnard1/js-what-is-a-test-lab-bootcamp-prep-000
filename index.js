08456e3e3bef2c784099592a744ef482a99e17a8
Alana Barnard <alanabarnard5@gmail.com>
2020-07-11 19:18:33 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 // write your code below!
 var name = "Susan"
 var height = 74
+if (74<40) {console.log("74 is less than 40")}
+else {console.log("74 is less than 40")}
+
 var message = ` is ${height} inches tall`
 
 module.exports = {name, height, message}
